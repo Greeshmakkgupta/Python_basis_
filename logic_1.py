56d92a1411215671cd62241e6db7c505f65c2cf1
Greeshmakkgupta <52143219+Greeshmakkgupta@users.noreply.github.com>
2019-08-07 08:38:23 +0530

    Add files via upload

---
+++ logic_1.py
@@ -0,0 +1,18 @@
+r=int('101',10)
+s=int('101')
+w=int('101',2)
+x=int('ff',16)
+t=float('1.1')
+print(type(r),r)
+print(type(w),w)
+print(type(x),x)
+print(type(s),s)
+print(type(t))
+
+output
+
+<class 'int'> 101
+<class 'int'> 5
+<class 'int'> 255
+<class 'int'> 101
+<class 'float'>
